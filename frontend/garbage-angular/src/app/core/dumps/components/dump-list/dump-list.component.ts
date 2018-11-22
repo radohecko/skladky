f78365b0cc6951b47374d01fc3abba749902c549
Radoslav Hečko <radohecko@users.noreply.github.com>
2018-11-22 19:18:31 +0100

    Routing and filtering dumps

--- frontend/garbage-angular/src/app/core/dumps/components/dump-list/dump-list.component.ts
+++ frontend/garbage-angular/src/app/core/dumps/components/dump-list/dump-list.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Dump } from 'src/app/shared/interfaces/dump';
 import { DumpsService, toggleOptionsLeft, toggleOptionsRight } from '../../services/dumps.service';
 import { Subscription } from 'rxjs';
-import { ToggleGroupOption } from 'src/app/shared/components/toggle-buttons/toggle-buttons.component';
+import { ToggleGroupOption, ToggleGroupValue } from 'src/app/shared/components/toggle-buttons/toggle-buttons.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dump-list',
@@ -12,6 +13,7 @@ import { ToggleGroupOption } from 'src/app/shared/components/toggle-buttons/togg
 export class DumpListComponent implements OnInit {
 
   dumps: Dump[];
+  filteredDumps: Dump[];
   dumpsSubscription: Subscription;
 
   toggleOptionsLeft: ToggleGroupOption[] = toggleOptionsLeft;
@@ -19,17 +21,41 @@ export class DumpListComponent implements OnInit {
   optionSelectedValueLeft = 'List';
   optionSelectedValueRight = 'All';
 
-  constructor(private dumpsService: DumpsService) { }
+  constructor(private dumpsService: DumpsService, private router: Router) { }
 
   ngOnInit() {
     this.dumpsService.getDumps();
     this.dumpsSubscription = this.dumpsService.dumpsObservable$.subscribe(data => {
       this.dumps = data;
+      this.filteredDumps = this.dumps;
     });
   }
 
-  onSelectedValueChange($event) {
-    console.log($event);
+  filterByStatus(status: string) {
+    if (status !== 'All') {
+      this.filteredDumps = this.dumps.filter(dump => dump.status === status);
+    } else {
+      this.filteredDumps = this.dumps;
+    }
+  }
+
+  onSelectedValueChange($event: ToggleGroupValue) {
+    switch ($event) {
+      case 'List':
+      this.router.navigate(['/dumps/list']);
+        break;
+      case 'Map':
+      this.router.navigate(['/dumps/map']);
+        break;
+      case 'All':
+      case 'Pending':
+      case 'Resolved':
+      case 'In Process':
+        this.filterByStatus($event);
+        break;
+      default:
+        break;
+    }
   }
 
 }
--- frontend/garbage-angular/src/app/core/dumps/components/dump-map/dump-map.component.ts
+++ frontend/garbage-angular/src/app/core/dumps/components/dump-map/dump-map.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ToggleGroupOption } from 'src/app/shared/components/toggle-buttons/toggle-buttons.component';
-import { toggleOptionsLeft, toggleOptionsRight } from '../../services/dumps.service';
+import { ToggleGroupOption, ToggleGroupValue } from 'src/app/shared/components/toggle-buttons/toggle-buttons.component';
+import { toggleOptionsLeft, toggleOptionsRight, DumpsService } from '../../services/dumps.service';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Dump } from 'src/app/shared/interfaces/dump';
 
 @Component({
   selector: 'app-dump-map',
@@ -9,17 +12,50 @@ import { toggleOptionsLeft, toggleOptionsRight } from '../../services/dumps.serv
 })
 export class DumpMapComponent implements OnInit {
 
+  dumps: Dump[];
+  filteredDumps: Dump[];
+  dumpsSubscription: Subscription;
+
   toggleOptionsLeft: ToggleGroupOption[] = toggleOptionsLeft;
   toggleOptionsRight: ToggleGroupOption[] = toggleOptionsRight;
   optionSelectedValueLeft = 'Map';
   optionSelectedValueRight = 'All';
 
-  constructor() { }
+  constructor(private dumpsService: DumpsService, private router: Router) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.dumpsService.getDumps();
+    this.dumpsSubscription = this.dumpsService.dumpsObservable$.subscribe(data => {
+      this.dumps = data;
+      this.filteredDumps = this.dumps;
+    });
+  }
 
-  onSelectedValueChange($event) {
-    console.log($event);
+  filterByStatus(status: string) {
+    if (status !== 'All') {
+      this.filteredDumps = this.dumps.filter(dump => dump.status === status);
+    } else {
+      this.filteredDumps = this.dumps;
+    }
+  }
+
+  onSelectedValueChange($event: ToggleGroupValue) {
+    switch ($event) {
+      case 'List':
+      this.router.navigate(['/dumps/list']);
+        break;
+      case 'Map':
+      this.router.navigate(['/dumps/map']);
+        break;
+      case 'All':
+      case 'Pending':
+      case 'Resolved':
+      case 'In Process':
+        this.filterByStatus($event);
+        break;
+      default:
+        break;
+    }
   }
 
 }
