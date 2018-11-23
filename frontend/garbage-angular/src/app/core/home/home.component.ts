36f05502c705ae16bfecec37991bf2cbb381dd21
marmanova3 <marmanova3@uniba.sk>
2018-11-23 13:32:08 +0100

    homapage

--- frontend/garbage-angular/src/app/core/home/home.component.ts
+++ frontend/garbage-angular/src/app/core/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DumpAddComponent } from '../dumps/components/dump-add/dump-add.component';
+import { MatDialog } from '@angular/material';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
   }
 
+  onOpenAddReport() {
+    const dialogRef = this.dialog.open(DumpAddComponent, {
+      width: '650px'
+    });
+  }
 }
