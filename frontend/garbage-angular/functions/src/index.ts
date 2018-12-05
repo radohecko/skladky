2cf31c761980094bd23d5915f34ee2d6e9723630
Radoslav Hečko <radohecko@users.noreply.github.com>
2018-12-05 11:26:14 +0100

    Email Cloud function

--- frontend/garbage-angular/functions/src/index.ts
+++ frontend/garbage-angular/functions/src/index.ts
@@ -1,8 +1,45 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as sendGrid from '@sendgrid/mail';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/functions/write-firebase-functions
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
+sendGrid.setApiKey(firebaseConfig.sendGrid.key);
+
+export const firestoreEmail = functions.firestore
+  .document('dumps/{dumpId}/')
+  .onUpdate(event => {
+
+    const dumpId = event.params.dumpId;
+    const db = admin.firestore();
+
+    return db.collection('dumps').doc(dumpId)
+      .get()
+      .then(doc => {
+
+        const dump = doc.data()
+
+        if (dump.email !== '') {
+          const msg = {
+            to: dump.email,
+            from: 'forestdumps2018@gmail.com',
+            subject: 'Resolved dump report',
+            text: `Hey ${dump.email}. Dump was resolved!!! Thank you for making earth cleaner.`,
+  
+            // TODO: create custom template
+            // custom templates
+            templateId: '',
+            substitutionWrappers: ['{{', '}}'],
+            substitutions: {
+              name: dump.email
+              // and other custom properties here
+            }
+          };
+  
+          return sendGrid.send(msg)
+        }
+      })
+      .then(() => console.log('email sent!'))
+      .catch(err => console.log(err));
+  });
