f66060cba044d724266078661a00c472f90fbd87
Radoslav Hečko <radohecko@users.noreply.github.com>
2018-12-09 20:31:25 +0100

    Build

--- frontend/garbage-angular/src/environments/environment.prod.ts
+++ frontend/garbage-angular/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDz5YPJ-Mi0K3mNOdAUf63wmifKRdNMih8',
+    authDomain: 'forest-dump.firebaseapp.com',
+    databaseURL: 'https://forest-dump.firebaseio.com',
+    projectId: 'forest-dump',
+    storageBucket: 'forest-dump.appspot.com',
+    messagingSenderId: '1047387364814'
+  }
 };
--- frontend/garbage-angular/src/environments/environment.ts
+++ frontend/garbage-angular/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: 'AIzaSyDz5YPJ-Mi0K3mNOdAUf63wmifKRdNMih8',
     authDomain: 'forest-dump.firebaseapp.com',
